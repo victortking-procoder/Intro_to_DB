a0a3b9854f80bedc375deccd14d90990ef23f89a
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-28 14:23:17 +0100

    inserted three more data into customers table

---
+++ task_6.sql
@@ -0,0 +1,6 @@
+USE alx_book_store;
+
+INSERT INTO customer
+VALUES ("Blessing Malik", "bmalik@sandtech.com", "126 Happiness  Ave."),
+("Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+("Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
