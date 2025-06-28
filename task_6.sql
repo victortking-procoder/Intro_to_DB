ab4e93197fc4692a6af05c4316be9566e7037698
victortking-procoder <fredrickvictor01@outlook.com>
2025-06-28 14:31:11 +0100

    inserted three more data into customer table

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "126 Happiness  Ave."):
+VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."):
 
 INSERT INTO customer (customer_id, customer_name, email, address)
 (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave.");
